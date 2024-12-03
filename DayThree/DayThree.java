1d7509ff9371f439b284a727f4b4d2d92d0e1b36
aoife <amulvenna10@qub.ac.uk>
2024-12-03 22:48:17 +0000

    day 3 part 1

--- DayThree/DayThree.java
+++ DayThree/DayThree.java
@@ -1,22 +1,41 @@
 import java.io.*;
-import java.util.Scanner; 
+import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Collections;
 import java.lang.Math;
 
 class DayThree {
-    public static void main(String[] args) throws Exception{
-     File file = new File("/Users/aoifem/advent_2024/DayThree/DayThreeInput.txt");
-      Scanner reader = new Scanner(file);
-      while (reader.hasNextLine()) {
-        String data = reader.nextLine();
-        System.out.println(data);
-      }
-      reader.close();
-    } catch (FileNotFoundException e) {
-      System.out.println("An error occurred.");
-      e.printStackTrace();
+    public static void main(String[] args) throws Exception {
+        try {
+            File file = new File("/Users/aoifem/advent_2024/DayThree/DayThreeInput.txt");
+            Scanner reader = new Scanner(file);
+            int total = 0;
+            while (reader.hasNextLine()) {
+                String data = reader.nextLine();
+
+                String regex = "mul\\(";
+
+                String[] myArray = data.split(regex);
+
+                for (String rawLine: myArray) {
+                    String closeBrRegex = "\\)";
+                    String[] arrayTwo = rawLine.split(closeBrRegex, 2);
+                    String numberLine = arrayTwo[0];
+                    String[] arrayThree = numberLine.split(",");
+                    if (arrayThree.length == 2 && arrayThree[1].matches("^-?\\d+$")) {
+                        total += Integer.parseInt(arrayThree[0]) * Integer.parseInt(arrayThree[1]);
+                    }
+                }
+
+            }
+            reader.close();
+            System.out.println(total);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
     }
-        
 }
